b636c38dce08f8b097e2f20603a6d2c779ff8df2
Uncle Bogdan <85982863+freshe4qa@users.noreply.github.com>
2024-11-12 16:40:04 +0200

    Update axone.sh

--- axone.sh
+++ axone.sh
@@ -52,20 +52,20 @@ apt install curl iptables build-essential git wget jq make gcc nano tmux htop nv
 
 # install go
 sudo rm -rf /usr/local/go
-curl -Ls https://go.dev/dl/go1.22.8.linux-amd64.tar.gz | sudo tar -xzf - -C /usr/local
-eval $(echo 'export PATH=$PATH:/usr/local/go/bin' | sudo tee /etc/profile.d/golang.sh)
-eval $(echo 'export PATH=$PATH:$HOME/go/bin' | tee -a $HOME/.profile)
+curl -L https://go.dev/dl/go1.22.7.linux-amd64.tar.gz | sudo tar -xzf - -C /usr/local
+echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >> $HOME/.profile
+source .profile
 
 # download binary
 cd $HOME && rm -rf axoned
-git clone https://github.com/axone-protocol/axoned
+git clone https://github.com/axone-protocol/axoned.git
 cd axoned
 git checkout v10.0.0
-make build
+make install
 
 # config
-axoned config chain-id $AXONE_CHAIN_ID
-axoned config keyring-backend test
+#axoned config chain-id $AXONE_CHAIN_ID
+#axoned config keyring-backend test
 
 # init
 axoned init $NODENAME --chain-id $AXONE_CHAIN_ID
@@ -78,8 +78,8 @@ curl -L https://snapshots-testnet.nodejumper.io/axone/addrbook.json > $HOME/.axo
 sed -i -e "s|^minimum-gas-prices *=.*|minimum-gas-prices = \"0.001uaxone\"|" $HOME/.axoned/config/app.toml
 
 # set peers and seeds
-SEEDS=""
-PEERS="8331463d7bc974e49eab3bd953bceed4d66f4104@65.109.117.113:28156,e5abfa9c78479a1ea35f54d7bfb1a25921b6f387@65.109.84.33:20056,7d610ad1f75857c01b45e8b6f25d73457a543611@142.132.209.236:17656,bed4fb66aa7badfc224dd6ccc4a3cc0ab214cd7d@74.220.23.137:26656,831e3417253e14e440a1cc2782d4bbfb25596873@188.40.85.207:14156,d453ea33398978c62b45b34ea6acecc4aab3a1ed@5.9.87.231:26656,adb5e004b95e6db7041e68af878cf8b8bada0ec3@141.94.143.203:55156,a77f5b85fb8969be5540acf3a8643d6a2f07c776@84.201.135.7:26656,a98484ac9cb8235bd6a65cdf7648107e3d14dab4@95.217.74.22:13656,67faf9297f4dadf10f46ebe2a45bbe55118622c6@65.108.233.28:36656,910e678dbd20955652b8a2942fd173e54d9e95c1@65.21.233.188:17656,1d4256993f1c08571c3bf4e9362246b736b12125@65.108.199.79:26103,67bd9e88011970b3ff5652b75065c8956d0693ea@37.27.184.10:26656,4662e56afcec47fc7eacca4de4a8ca382d4b97b7@65.109.92.163:2020,4569842347acc9204971c243315f1839e89b1cc7@65.108.111.226:31656,e8838b99dabdbc60d776b359f9929ecbaf7ba82f@65.109.93.58:20056,c9d8b4ea3d4b5b95cb122f9778edbb6b399f9deb@94.130.138.48:45656,809d0a8984a2c293d263931a32f6d08e4277a106@65.109.65.210:33656,c27e8cb52aa588431e39f5c8b32c30850a228b8b@5.9.116.21:20056,8e7dc1bc3c9dc2106e077e6bbd48f3790dd5c934@144.91.115.146:26656"
+SEEDS="3f472746f46493309650e5a033076689996c8881@axone-testnet.rpc.kjnodes.com:13659"
+PEERS="d453ea33398978c62b45b34ea6acecc4aab3a1ed@5.9.87.231:26656,8ea05a621d5fdfbda4192ae8369f289ef04c04ba@78.46.74.23:25656,0f219e851af7cd4fca087bff354dc407a76c3ce2@144.76.112.58:24656,d89568d0fda69b1951a433f5f5ff887213a41305@5.9.73.170:17656,a1da085f304cda7a6bf42d0e025ca84764aeef7f@142.132.152.46:18656,d8b4abd10feb608db9ed6dd3926dfa85eed3c498@43.131.53.203:26656,9614c853f70a0010215587a31677b99144e96507@152.228.211.19:26656,5e7747650adbed323baff71523b4cdeaf6d8a57c@77.68.82.101:26655,aa968e094c5530203232d4a1c6d20fd172bba586@135.181.79.242:26656,4569842347acc9204971c243315f1839e89b1cc7@65.108.111.226:31656,839bc9d8aea9a187b59df6f8e42a16f8e6d875a1@65.21.47.120:34656,24871048be1e61ea1df2e06ed7ed3e5cd829c92a@65.109.112.148:10096,e2eeb94a734de5d4cdca408ffb3ef675183105d5@65.109.83.40:29856,910e678dbd20955652b8a2942fd173e54d9e95c1@65.21.233.188:17656,7bcb9d1682d261f6336035ed436ba868bdace0ef@144.217.68.182:21956,582dbc3df1128a55a3ced347a6ad7e57d42e3d8c@136.243.13.36:17656,ab93659fbefaa8e5ede54b1abeaa747682aba59e@74.208.16.201:26646,e333cd668f43ff0994fb1e5aad1315061120af74@213.199.44.243:17656,a98484ac9cb8235bd6a65cdf7648107e3d14dab4@95.217.74.22:13656,cee4251c2ee5f24ed0e180dc1d3298d66ebe13e8@49.12.150.42:26736"
 sed -i -e "s/^seeds *=.*/seeds = \"$SEEDS\"/; s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $HOME/.axoned/config/config.toml
 
 # disable indexing
